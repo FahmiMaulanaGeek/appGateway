2b9ee3c9ae01f07d0ea47281625642ece0656c9b
Fahmi Maulana <fahmimaulanaforwork@gmail.com>
2022-06-11 21:48:48 +0700

    Added My Project

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\Gateway;
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\SMSController;
+use App\http\Controllers\TwilioSMSController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('sendSMS', [TwilioSMSController::class, 'index']);
